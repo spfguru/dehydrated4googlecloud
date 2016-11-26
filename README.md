9429b0bbd58ec3ef15657179761fd9658301f581
Oliver Brahmstädt <brahmstaedt@softwarebetrieb.de>
2016-11-26 18:37:15 +0100

    Added hook and README

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# dehydrated4googlecloud
-Hook for "dehydrated" to automate letsencrypt certificate renewal on the Google Cloud platform
+# Google Cloud hook for `dehydrated`
+
+This is a hook for the [Let's Encrypt](https://letsencrypt.org/) ACME client [dehydrated](https://github.com/lukas2511/dehydrated) (previously known as `letsencrypt.sh`) that allows you to use [Google Cloud DNS](https://cloud.google.com/dns/docs/) records to respond to `dns-01` challenges as well as uploading the certificates to [Google Cloud Platform HTTPS load balancers](https://cloud.google.com/compute/docs/load-balancing/http/). This hook requires the Google Cloud SDK (aka the gcloud command-line tool).
+
+## Installation
+
+```
+$ cd ~
+$ git clone https://github.com/lukas2511/dehydrated
+$ cd dehydrated
+$ mkdir hooks
+$ git clone https://github.com/spfguru/dehydrated4googlecloud hooks/google-cloud
+```
+
+Make sure you have the latest version of the [Google Cloud SDK](https://cloud.google.com/sdk/downloads) installed. 
+
+## Configuration
+
+This hook uses the gcloud command-line tool and fascilitates the default project and account information. Check ```gcloud info``` to see, what this is set to. Also, your account needs to have "editor" permissions in the current project. This project needs to host your DNS zone for the domain (or a subdomain) you want to get a Let's Encrypt certificate for. Also, if you use the Google Cloud HTTPS load balancers, these have to be in the same project as well. Only required if you wish this hook to update the created certificates automatically. 
+
+Also you need to change the following settings in your dehydrated config (original value commented out):
+```
+# Which challenge should be used? Currently http-01 and dns-01 are supported
+#CHALLENGETYPE="http-01"
+CHALLENGETYPE="dns-01"
+
+# Default keysize for private keys (default: 4096)
+#KEYSIZE="4096"
+# Google Cloud currently only supports up to 2048 bit key length
+KEYSIZE="2048"
+``` 
+
+If you use Google Cloud HTTPS load balancers, you need to align your setup of target proxies with how you create the certificates. All domains served by a target proxy have to be in the same certificate. If that is more than one, you cannot use the -d command line option of dehydrated. Instead you have to create a domains.txt file. The following example assumes you have two target proxies; one serving requests for example.com and www.example.com. And the second one serving wwwtest.example.com:
+
+domains.txt
+``` 
+example.com www.example.com
+wwwtest.example.com
+``` 
+
+After certificate have been created, this hook will add the newly created certificates to the Google Cloud HTTPS load balancer; no existing SSL certificates will be overwritten. In order to activate the new certificates, the so called target proxy has to point to the new certifiacte. You need to do that manually, unless your target proxy name adheres to the following naming convention:
+
+```
+https-proxy-DOMAIN-NAME
+```
+
+Where DOMAIN-NAME is the first domain name of each line in the domains.txt, having each '.' replaced by a '-'. Given the above sample domains.txt, the hook would look for https-proxy-example-com and https-wwwtest-proxy-example-com. Keep in mind that the www.example.com domain would be served from the https-proxy-example-com, thus no dedicated target proxy exists for that.
+
+## Usage
+
+```
+$ ./dehydrated -c -t dns-01 -k 'hooks/google-cloud/hook.sh'
+```
+
+The ```-t dns-01``` part can be skipped, if you have set this challenge type in your config already. Same goes for the ```-k 'hooks/google-cloud/hook.sh'``` part, when set in the config as well.
+
---
+++ hook.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+
+function deploy_challenge {
+    # This hook is called once for every domain that needs to be
+    # validated, including any alternative names you may have listed.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The domain name (CN or subject alternative name) being
+    #   validated.
+    # - TOKEN_FILENAME
+    #   The name of the file is irrelevant for the DNS challenge, yet still provided 
+    # - TOKEN_VALUE
+    #   The token value that needs to be served for validation. For DNS
+    #   validation, this is what you want to put in the _acme-challenge
+    #   TXT record. For HTTP validation it is the value that is expected
+    #   be found in the $TOKEN_FILENAME file.
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+
+    echo;
+    echo "Deploying challenge for domain $DOMAIN"
+
+    managedZones=`gcloud dns managed-zones list --format="value(dnsName,name)"`
+    managedZones=${managedZones//$'\t'/,}
+
+    mainDomainFound=false
+    for managedZone in $managedZones; do 
+        IFS=$',' read dnsDomain zonename <<< $managedZone
+
+        if [[ "$DOMAIN." == "$dnsDomain" || "$DOMAIN." == *".$dnsDomain" ]]; then
+            mainDomainFound=true
+            break
+        fi
+    done
+
+    if ! $mainDomainFound ; then
+        echo "Domain $DOMAIN not hosted in this Google Cloud DNS project."
+        exit 1
+    fi
+
+    test -f transaction.yaml && rm transaction.yaml
+    gcloud dns record-sets transaction start --zone $zonename
+
+    # remove token, if it exists
+    existingRecord=`gcloud dns record-sets list --name "_acme-challenge.$DOMAIN." --type TXT --zone $zonename --format='value(name,rrdatas[0],ttl)'`
+    existingRecord=${existingRecord//$'\t'/,}
+    IFS=$',' read existingName existingRrdata existingTtl <<< $existingRecord
+
+    # Replace threefold """ with single "
+    existingRrdata=${existingRrdata//$'"""'/''}
+
+    if [ "$existingName" == "_acme-challenge.$DOMAIN." ]; then
+        gcloud dns record-sets transaction remove "$existingRrdata" --name $existingName --type TXT --ttl $existingTtl --zone $zonename
+    fi
+
+    gcloud dns record-sets transaction add --name "_acme-challenge.$DOMAIN." --ttl 300 --type TXT "$TOKEN_VALUE" --zone $zonename
+    gcloud dns record-sets transaction describe --zone $zonename
+
+    changeID=$(gcloud dns record-sets transaction execute --zone $zonename --format='value(id)')
+
+    status=$(gcloud dns record-sets changes describe $changeID --zone $zonename --format='value(status)')
+    echo -n "Checking execution status of this transaction (can easily take 2-5 minutes): "
+    until [[ "$status" = "done" ]]; do
+        echo -n "$status"
+        sleep 1
+        echo -n "."
+        sleep 1
+        echo -n "."
+        sleep 1
+        echo -n "."
+        sleep 1
+        echo -n "."
+        status=$(gcloud dns record-sets changes describe $changeID --zone $zonename --format='value(status)')
+    done
+    echo "done"
+
+    # Even if the transaction is executed, the results may not be available in the DNS servers yet
+    echo "Verifying results on live DNS servers:"
+    for nameserver in $(dig $dnsDomain NS +short); do 
+        echo -n "$nameserver " 
+        nsresult=$(dig _acme-challenge.$DOMAIN TXT @$nameserver +short)
+        # nsresult comes with the TXT RR in double quotes - remove those
+        nsresult=${nsresult//$'"'/''}
+        until [[ "$nsresult" = "$TOKEN_VALUE" ]]; do
+            echo -n "pending"
+            sleep 1
+            echo -n "."
+            sleep 1
+            echo -n "."
+            sleep 1
+            echo -n "."
+            sleep 1
+            echo -n "."
+            nsresult=$(dig _acme-challenge.$DOMAIN TXT @$nameserver +short)
+            # nsresult comes with the TXT RR in double quotes - remove those
+            # TODO DRY: move to dedicated function
+            nsresult=${nsresult//$'"'/''}
+        done
+        echo "done"
+    done
+
+    echo "Sleeping for another 30 seconds to avoid timing conflicts"
+    sleep 30
+}
+
+
+function clean_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+
+    echo;
+    echo "Cleaning challenge for domain $DOMAIN"
+    # This hook is called after attempting to validate each domain,
+    # whether or not validation was successful. Here you can delete
+    # files or DNS records that are no longer needed.
+    #
+    # The parameters are the same as for deploy_challenge.
+
+    # TODO DRY: move the following block into a function!
+    managedZones=`gcloud dns managed-zones list --format="value(dnsName,name)"`
+    managedZones=${managedZones//$'\t'/,}
+
+    mainDomainFound=false
+    for managedZone in $managedZones; do 
+        IFS=$',' read dnsDomain zonename <<< $managedZone
+
+        if [[ "$DOMAIN." == "$dnsDomain" || "$DOMAIN." == *".$dnsDomain" ]]; then
+            mainDomainFound=true
+            break
+        fi
+    done
+
+    if ! $mainDomainFound ; then
+        echo "Domain $DOMAIN not hosted in this Google Cloud DNS project."
+        exit 1
+    fi
+
+    test -f transaction.yaml && rm transaction.yaml
+    gcloud dns record-sets transaction start --zone $zonename
+
+    existingRecord=`gcloud dns record-sets list --name "_acme-challenge.$DOMAIN." --type TXT --zone $zonename --format='value(name,rrdatas[0],ttl)'`
+    existingRecord=${existingRecord//$'\t'/,}
+    IFS=$',' read existingName existingRrdata existingTtl <<< $existingRecord
+
+    # Replace threefold """ with singe "
+    existingRrdata=${existingRrdata//$'"""'/''}
+
+    gcloud dns record-sets transaction remove "$existingRrdata" --name $existingName --type TXT --ttl $existingTtl --zone $zonename
+    gcloud dns record-sets transaction execute --zone $zonename
+}
+
+
+function deploy_cert {
+    # This hook is called once for each certificate that has been
+    # produced. Here you might, for instance, copy your new certificates
+    # to service-specific locations and reload the service.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain name, i.e. the certificate common
+    #   name (CN).
+    # - KEYFILE
+    #   The path of the file containing the private key.
+    # - CERTFILE
+    #   The path of the file containing the signed certificate.
+    # - FULLCHAINFILE
+    #   The path of the file containing the full certificate chain.
+    # - CHAINFILE
+    #   The path of the file containing the intermediate certificate(s).
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+
+    echo;
+    echo "Deploying certificate for $DOMAIN from $KEYFILE and $FULLCHAINFILE"
+    canonicalname=$(sed 's:\.:-:g' <<< $DOMAIN)
+    certname=$canonicalname-$(date +%s)
+    httpsproxyname=https-proxy-$canonicalname
+
+    gcloud beta compute ssl-certificates create $certname --certificate $FULLCHAINFILE --private-key $KEYFILE --description "$DOMAIN"
+
+
+    if [ "$httpsproxyname" == "$(gcloud compute target-https-proxies describe $httpsproxyname --format='value(name)' &2> /dev/null)" ]; then
+        gcloud compute target-https-proxies update $httpsproxyname --ssl-certificate $certname
+    else
+        echo "====================================================================================================="
+        echo "WARNING: Unable to find https target proxy named '$httpsproxyname' - no automatic update performed";
+        echo "YOU have to update your target proxy manually and set the SSL certificate to '$certname'"
+        echo "Go to https://console.cloud.google.com/networking/loadbalancing/advanced/targetHttpsProxies/"
+        echo "OR run the following command: (change \$MY_HTTPS_PROY_NAME to your actual proxy name)"
+        echo "gcloud compute target-https-proxies update \$MY_HTTPS_PROY_NAME --ssl-certificate $certname"
+        echo "====================================================================================================="
+    fi
+}
+
+function unchanged_cert {
+    # This hook is called once for each certificate that is still valid at least 30 days
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain name, i.e. the certificate common
+    #   name (CN).
+    # - KEYFILE
+    #   The path of the file containing the private key.
+    # - CERTFILE
+    #   The path of the file containing the signed certificate.
+    # - FULLCHAINFILE
+    #   The path of the file containing the full certificate chain.
+    # - CHAINFILE
+    #   The path of the file containing the intermediate certificate(s).
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+
+    echo "Certificate for domain $DOMAIN is still valid - no action taken"
+}
+
+
+HANDLER=$1; shift; $HANDLER $@
